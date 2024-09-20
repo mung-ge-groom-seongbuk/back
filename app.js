717cdee154ef80244da2d2cf906b14c7a4cba709
obongsik <zkfkdzhdo@naver.com>
2024-09-20 13:43:45 +0900

    feat : initial setting

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const http = require("http");
-const createServer = http.createServer((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/html" });
-	res.end("Hello World!");
-});
+//index.js
+const express = require('express') //③번 단계에서 다운받았던 express 모듈을 가져온다.
+const app = express() //가져온 express 모듈의 function을 이용해서 새로운 express 앱을 만든다. 🔥
+const port = 5000 
 
-createServer.listen(3000, () => {
-	console.log("Server Started!");
-});
+app.get('/', (req, res) => { //express 앱(app)을 넣고, root directory에 오면, 
+  res.send('Hello World!') //"Hello World!" 를 출력되게 해준다.
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}) //포트 5000번에서 이 앱을 실행한다.
